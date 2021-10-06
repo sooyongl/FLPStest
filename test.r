84e3c8ac8f91ecf4b77d8779afa831b7a57b0abd
Sooyong Lee <sooyongl09@utexas.edu>
2021-10-06 17:09:43 -0500

    Add testing the function

--- test.r
+++ test.r
@@ -1,13 +1,13 @@
 # rm(list = ls())
 for(i in fs::dir_ls("R", regexp = "r$")) source(i)
 
-memory.limit(50000)
+# memory.limit(50000)
+# rstan_options(auto_write = TRUE)
+# options(mc.cores = parallel::detectCores())
 
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-
+# data generation ---------------------------------------------------------
 parsFromMod <- list(
-  N = 1000, # 5308
+  N = 1000, # sample size
   R2Y = 0.2, ## from app
   omega = 0.2,
   tau0 = 0.13, ## from paper
@@ -19,8 +19,22 @@ parsFromMod <- list(
 )
 
 sdat <- do.call("makeDat", parsFromMod)
-# system.time(sdat <- do.call("makeDat", parsFromMod))
+# or
+# sdat <- makeDat(N = 1000, R2Y = 0.2, omega = 0.2, tau0 = 0.13, tau1 = -0.06, 
+#                lambda = 10, R2eta = 0.5, nsec = 10, lvmodel = "rasch")
 
-### NOT RUN
-system.time(fit <- rstan::stan("R/psRasch.stan",data=sdat))
+# test -----------------------------------------------------------------
+measMod <- with(sdat, 
+                glmer(grad~X[studentM,]+(1|section)+(1|studentM),
+                      family=binomial))
+ 
+# method 1
+mod0 <- with(sdat,lm(Y[Z==0]~X[Z==0,]))
+mod1 <- with(sdat,lm(Y[Z==1]~X[Z==1,]))
+# 
+## only two covariates so...
+mean((coef(mod1)[-1]-coef(mod0)[-1])/fixef(measMod)[-1])
+
+# run stan --------------------------------------------------------------
+# system.time(fit <- rstan::stan("R/psRasch.stan",data=sdat))
 
