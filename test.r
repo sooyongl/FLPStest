e8a2308c61f628b570a15209773f1df873dd01ab
Sooyong Lee <sooyongl09@utexas.edu>
2021-10-07 11:43:30 -0500

    Update test.r

--- test.r
+++ test.r
@@ -3,7 +3,7 @@ for(i in fs::dir_ls("R", regexp = "r$")) source(i)
 
 # memory.limit(50000)
 # rstan_options(auto_write = TRUE)
-# options(mc.cores = parallel::detectCores())
+# options(mc.cores = parallel::detectCores() - 2)
 
 # data generation ---------------------------------------------------------
 parsFromMod <- list(
@@ -19,6 +19,7 @@ parsFromMod <- list(
 )
 
 sdat <- do.call("makeDat", parsFromMod)
+str(sdat)
 # or
 # sdat <- makeDat(N = 1000, R2Y = 0.2, omega = 0.2, tau0 = 0.13, tau1 = -0.06, 
 #                lambda = 10, R2eta = 0.5, nsec = 10, lvmodel = "rasch")
@@ -36,5 +37,20 @@ mod1 <- with(sdat,lm(Y[Z==1]~X[Z==1,]))
 mean((coef(mod1)[-1]-coef(mod0)[-1])/fixef(measMod)[-1])
 
 # run stan --------------------------------------------------------------
-# system.time(fit <- rstan::stan("R/psRasch.stan",data=sdat))
+system.time(fit <- rstan::stan("R/psRasch.stan",data=sdat))
+# saveRDS(fit, "rstan_fit.rds")
+
+fit <- readRDS("rstan_fit.rds")
+
+print(fit, pars = 'b1')
+
+
+stan_hist(fit, bins=40)
+
+fit.ggmcmc <- ggs(fit)
+theme_set(theme_bw())
+
+fit.ggmcmc %>% filter(Parameter == "studEff[1]") %>% ggs_traceplot(.)
+fit.ggmcmc %>% filter(Parameter == "studEff[1]") %>% ggs_density(.)
+fit.ggmcmc %>% filter(Parameter == "studEff[1]") %>% ggs_caterpillar(.)
 
