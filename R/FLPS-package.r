9301d6a8470e3d0df9ec72378f707d64fb86ac9a
Sooyong Lee <sooyongl09@utexas.edu>
2021-11-12 20:15:10 -0600

    Update FLPS-package.r

--- R/FLPS-package.r
+++ R/FLPS-package.r
@@ -1,20 +1,22 @@
 #' The 'FLPS' package.
 #'
+#' @name FLPS-package
 #' @title Fully latent principal stratification
 #' @description The FLPS package conducts bayesian analysis for fully latent principal stratification via rstan.
 #'
 #' @docType package
 #' @aliases FLPS
-#' @import Rcpp
-#' @import rstan
-#' @import coda
-#' @import methods
-#' @importFrom utils read.csv packageVersion head tail
-#' @importFrom MASS mvrnorm
+#' @author
 #'
 #' @references
 #' Sales, A. C., & Pane, J. F. (2019). The role of mastery learning in an intelligent tutoring system: Principal stratification on a latent variable. The Annals of Applied Statistics, 13(1), 420-443.
-#'
+#' @import Rcpp
+#' @import rstan
+# #' @import coda
+#' @import methods
+#' @importFrom utils read.csv packageVersion head tail
+#' @importFrom MASS mvrnorm
+#' @keywords package
 NULL
 
 # library(lavaan)
