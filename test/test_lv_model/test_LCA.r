939d555fc0b0443a7ce35cfdad49d4a81beea740
Sooyong Lee <sooyongl09@utexas.edu>
2021-12-15 11:29:35 -0600

    test models

--- test/test_lv_model/test_LCA.r
+++ test/test_lv_model/test_LCA.r
@@ -5,98 +5,111 @@ for(i in fs::dir_ls("R", regexp = "r$")) source(i); rm(i)
 source_funs <- ls()
 
 # LCA -----------------------------------------
-# for(i in 1:10) {
-#   # i = 1
-#
-#   true_eta <- c(rnorm(250), rnorm(250, 0.7))
-#   info <- parsForLVM(
-#     theta = true_eta,
-#     n_class = 2,
-#     n_indi  = 10,
-#     seperation = 0.9,
-#     data_type = "lca")
-#
-#   data_info <- generate.lca(info)
-#   # apply(data_info[data_info$class == 1, 1:10], 2, table)
-#   # apply(data_info[data_info$class == 2, 1:10], 2, table)
-#
-#   grad <- data_info[, 1:10] - 1
-#
-#   # idx <- which(!is.na(grad), arr.ind = T)
-#   # idx <- cbind(idx, 1:dim(idx)[1])
-#   # idx <- idx[order(idx[,1]), ]
-#   # nsecWorked <- nrow(idx)
-#   # nstud <- nrow(grad)
-#   # nsec <- ncol(grad)
-#   #
-#   # studentM <- idx[,1]
-#   # section <- idx[,2]
-#   # grad <- sapply(1:dim(idx)[1], function(n) grad[idx[n,1], idx[n,2]] )
-#   nstud <- 500
-#   nsec <- 10
-#   nworked <- sample(1:10,500,replace=TRUE,prob=dexp(1:10,rate=1/6))
-#   nsecWorked <- sum(nworked)
-#   studentM <- do.call("c", lapply(seq(500),function(n) rep(n,each=nworked[n])))
-#
-#   section <- do.call("c", lapply(seq(500),
-#                                  function(n) {
-#                                    sort(sample(1:10, nworked[n],
-#                                                replace = FALSE))}))
-#   ss <- cbind(studentM, section)
-#
-#   grad <- sapply(1:dim(ss)[1], function(n) grad[ss[n,1], ss[n,2]] )
-#
-#   studItem <- unname(table(studentM))
-#   # cumStudItem<- cumsum(studItem) - studItem[1]
-#   cumStudItem <- c(0, cumsum(studItem)[-length(studItem)])
-#
-#
-#   dt <- list(
-#     nclass = 2,
-#     studItem = studItem,
-#     cumStudItem = cumStudItem,
-#     nsecWorked = nsecWorked,
-#     nstud = nstud,
-#     nsec = nsec,
-#     #idx = idx,
-#     studentM = studentM,
-#     section = section,
-#     grad = grad
-#   )
-#
-#   # write_csv(data.frame(data_info), "test/test_lv_model/mplus/lca.csv", col_names = F)
-#   #
-#   # MplusAutomation::runModels("test/test_lv_model/mplus/lca.inp")
-#   # lca.out <- MplusAutomation::readModels("test/test_lv_model/mplus/lca.out")
-#   #
-#   # item.prob <- lca.out$parameters$probability.scale %>% filter(category == 2)
-#   # class.logit <- lca.out$parameters$unstandardized %>% filter(str_detect(paramHeader, "Means")) %>% pull(est)
-#   # class.prop <- (exp(class.logit) / (1 + exp(class.logit)))
-#
-#
-#   # stan_model <- paste(read_lines("test/test_lv_model/stan/casestudy_LCA.stan"),
-#   #                     collapse = "\n")
-#   stan_model <- paste(read_lines("test/test_lv_model/stan/LCA.stan"),
-#                       collapse = "\n")
-#   # cat(stan_model)
-#   lca.fit <- rstan::stan(
-#     model_code = stan_model,
-#     data = dt,
-#     iter = 4000,
-#     cores = 1,
-#     chains = 3
-#   )
-#
-#   res <- list(fit = lca.fit, sdat = dt, info = info, data_info = data_info)
-#   saveRDS(res, paste0("results/res_incompletedata_", "LCA","_",i ,".rds"))
-#
-# }
+for(i in 1:10) {
+  # i = 1
+
+  # true_eta <- c(rnorm(250), rnorm(250, 0.7))
+  # info <- parsForLVM(
+  #   theta = true_eta,
+  #   n_class = 2,
+  #   nsec  = 10,
+  #   seperation = 0.9,
+  #   data_type = "lca")
+  # 
+  # data_info <- generate.lca(info)
+  # apply(data_info[data_info$class == 1, 1:10], 2, table)
+  # apply(data_info[data_info$class == 2, 1:10], 2, table)
+
+  # grad <- data_info$resp[, 1:10]
+  # 
+  # idx <- which(!is.na(grad), arr.ind = T)
+  # idx <- cbind(idx, 1:dim(idx)[1])
+  # idx <- idx[order(idx[,1]), ]
+  # nsecWorked <- nrow(idx)
+  # nstud <- nrow(grad)
+  # nsec <- ncol(grad)
+  # 
+  # studentM <- idx[,1]
+  # section <- idx[,2]
+  # grad <- sapply(1:dim(idx)[1], function(n) grad[idx[n,1], idx[n,2]] )
+  # nstud <- 500
+  # nsec <- 10
+  # nworked <- sample(1:10,500,replace=TRUE,prob=dexp(1:10,rate=1/6))
+  # nsecWorked <- sum(nworked)
+  # studentM <- do.call("c", lapply(seq(500),function(n) rep(n,each=nworked[n])))
+  # 
+  # section <- do.call("c", lapply(seq(500),
+  #                                function(n) {
+  #                                  sort(sample(1:10, nworked[n],
+  #                                              replace = FALSE))}))
+  # ss <- cbind(studentM, section)
+  # 
+  # grad <- sapply(1:dim(ss)[1], function(n) grad[ss[n,1], ss[n,2]] )
+  # 
+  # studItem <- unname(table(studentM))
+  # # cumStudItem<- cumsum(studItem) - studItem[1]
+  # cumStudItem <- c(0, cumsum(studItem)[-length(studItem)])
+  # dt <- list(
+  #   nclass = 2,
+  #   studItem = studItem,
+  #   cumStudItem = cumStudItem,
+  #   nsecWorked = nsecWorked,
+  #   nstud = nstud,
+  #   nsec = nsec,
+  #   #idx = idx,
+  #   studentM = studentM,
+  #   section = section,
+  #   grad = grad
+  # )
+  
+  parsFromMod <- list(
+    N = 500, # sample size
+    R2Y = 0.2,
+    omega = 0.2,  # a1
+    tau0 = 0.13,  # b0
+    tau1 = -0.06, # b1
+    # lambda = 10,
+    lambda = 0.8,
+    R2eta = 0.2,
+    nsec = 20,
+    lvmodel = "lca" # tag for latent variable model
+  )
+  dt <- do.call("makeDat", parsFromMod)
+  dt$nclass <- 2
+  
+  # write_csv(data.frame(data_info), "test/test_lv_model/mplus/lca.csv", col_names = F)
+  #
+  # MplusAutomation::runModels("test/test_lv_model/mplus/lca.inp")
+  # lca.out <- MplusAutomation::readModels("test/test_lv_model/mplus/lca.out")
+  #
+  # item.prob <- lca.out$parameters$probability.scale %>% filter(category == 2)
+  # class.logit <- lca.out$parameters$unstandardized %>% filter(str_detect(paramHeader, "Means")) %>% pull(est)
+  # class.prop <- (exp(class.logit) / (1 + exp(class.logit)))
+
+
+  # stan_model <- paste(read_lines("test/test_lv_model/stan/casestudy_LCA.stan"),
+  #                     collapse = "\n")
+  stan_model <- paste(read_lines("test/test_lv_model/stan/LCA_indprop_test.stan"),
+                      collapse = "\n")
+  # cat(stan_model)
+  lca.fit <- rstan::stan(
+    model_code = stan_model,
+    data = dt,
+    iter = 5000,
+    cores = 1,
+    chains = 1
+  )
+
+  res <- list(fit = lca.fit, sdat = dt, info = info, data_info = data_info)
+  saveRDS(res, paste0("results/res_indiprop_", "LCA","_",i ,".rds"))
+
+}
 
 
 extract_res <- function(fit, data_info) {
 
   fit <- as.data.frame(fit)
-  fit$chain <- rep(c(1:3), each = 2000)
+  fit$chain <- rep(c(1:2), each = 2000)
 
   fit.chain1 <- colMeans(fit[fit$chain == 1, ])
   fit.chain2 <- colMeans(fit[fit$chain == 2, ])
@@ -208,16 +221,16 @@ saveRDS(param_list, "report/rds/res_combined_LCA_1129.rds")
   info <- parsForLVM(
     theta = true_eta,
     n_class = 2,
-    n_indi  = 10,
+    nsec  = 10,
     seperation = 0.9,
     data_type = "lca")
 
   data_info <- generate.lca(info)
-  table(data_info$class)/length(data_info$class)
+  
   # apply(data_info[data_info$class == 1, 1:10], 2, table)
   # apply(data_info[data_info$class == 2, 1:10], 2, table)
 
-  grad <- data_info[, 1:10] - 1
+  grad <- data_info$resp[, 1:10] - 1
 
   idx <- which(!is.na(grad), arr.ind = T)
   idx <- cbind(idx, 1:dim(idx)[1])
@@ -281,4 +294,3 @@ round(a1, 3)
 round(a1[1:250],3)
 
 matrix(a1, 500, 2)
-
--- test/test_model.r
+++ test/test_model.r
@@ -5,31 +5,33 @@ source_funs <- ls()
 
 memory.limit(50000)
 rstan_options(auto_write = TRUE)
+options(mc.cores = 8)
 
 file_list <- c(
-  "inst/stan/psIRT_scaling.stan",
-  "inst/stan/psGPCM_scaling.stan",
-  "inst/stan/psSEM_scaling.stan"
+  "inst/stan/psIRT_scaled.stan",
+  "inst/stan/psGPCM_scaled.stan",
+  "inst/stan/psSEM_scaled.stan",
+  "inst/stan/psLCA.stan"
 )
 lvmodel_list <- c("2PL", "GPCM", "sem","lca")
 #
 # model ---------------------------------------------------------
-stan_file_name <- file_list[3]
-lvmodel <- lvmodel_list[3]
+stan_file_name <- file_list[4]
+lvmodel <- lvmodel_list[4]
 
-for(l in seq(0.2,1, 0.2)) {
+# for(l in 0.6) { #seq(0.2,1, 0.2)) {
   for(i in 1:1) {
-    # l <- seq(0.2,1, 0.2)[5]
+    l <- 0.6 #seq(0.2,1, 0.2)[3]
     # i <- 1
     # -------------------------------------------------------------------------
     parsFromMod <- list(
       N = 500, # sample size
       R2Y = 0.2,
       omega = 0.2,  # a1
-      tau0 = 0.13,  # b0
-      tau1 = -0.06, # b1
+      tau0 = 0.4,  # b0
+      tau1 = -0.2, # b1
       # lambda = 10,
-      lambda = l,
+      lambda = 0.6,
       R2eta = 0.2,
       nsec = 20,
       lvmodel = lvmodel # tag for latent variable model
@@ -38,6 +40,9 @@ for(l in seq(0.2,1, 0.2)) {
 
     # data <- data.frame(Y=sdat$Y,Z=sdat$Z,sdat$X,eta=sdat$true_eta)
     # lm(Y ~ Z + x1 + x2 + Z*eta, data = data)
+
+    # glm(class ~ x1 + x2, data.frame(class = sdat$lv.rep$class-1, sdat$X),
+    #     family = "binomial")
 
     # sdat <- do.call("makeInpData", parsFromMod)
     #
@@ -53,15 +58,37 @@ for(l in seq(0.2,1, 0.2)) {
     #              lv_type = lvmodel)
     #
     # sdat <- sdat1@stan_data
+    sdat$nclass <- 2
 
-    fit <- stan(stan_file_name,data=sdat,iter=5000,warmup=2000,cores=1,chains=4)
+    fit <- stan(stan_file_name,data=sdat,iter=5000,warmup=2000,chains=1)
+
+    # stan_file_noinfo <- "inst/stan/psIRT_scaled_noinfo.stan"
+    # fit <- stan(stan_file_noinfo,data=sdat,iter=10000,warmup=2000,chains=4)
+
+    # print(fit, c("b1"))
+    # print(fit, c("lambda[2]","lambda[3]","lambda[4]","lambda[5]"))
+    #
+    # stan_vb <- stan_model(file = stan_file_noinfo)
+    # vb_fit <-
+    #   vb(
+    #     stan_vb,
+    #     data = sdat,
+    #     iter = 15000,
+    #     elbo_samples = 1000,
+    #     algorithm = c("meanfield"),
+    #     # imporance_resampling = T,
+    #     output_samples = 10000,
+    #     tol_rel_obj = 0.00001
+    #   )
+    # print(vb_fit, c("b1"))
+
     res <- list(fit = fit, sdat = sdat)
-    saveRDS(res, paste0("results/res_", l, parsFromMod$lvmodel,"_",i ,".rds"))
+    saveRDS(res, paste0("results/res_", l,"_", parsFromMod$lvmodel,i ,".rds"))
 
     print(i)
+    gc()
   }
-}
-
+# }
 
 # -------------------------------------------------------------------------
 # -------------------------------------------------------------------------
@@ -73,11 +100,13 @@ for(l in seq(0.2,1, 0.2)) {
 res_list <- fs::dir_ls("results", regexp = "rds$")
 
 res_list <- res_list[str_detect(res_list, "2PL")]
-# res_list <- res_list[str_detect(res_list, "complete")]
+res_list <- res_list[str_detect(res_list, "_info[1-9]")]
 
 param_list <- vector("list", length(res_list))
 for(i in 1:length(res_list)) {
   model_type <- str_split(res_list[i], "_", simplify = T)[2]
+  scaley <- str_split(res_list[i], "_", simplify = T)[3]
+
   res <- readRDS(res_list[i])
 
   fit <- res$fit
@@ -85,9 +114,14 @@ for(i in 1:length(res_list)) {
 
   # item param
   df.fit <- as.data.frame(fit)
-  df.fit$chain <- rep(c(1:4), each = 3000)
+  df.fit$chain <- rep(c(1:4), each = 8000)
   df.fit <- df.fit %>% group_by(chain)
   #
+  # df.fit %>%
+  #   select(matches("^nu|^p|^b00|^b01|betaY|betaU")) %>%
+  #   summarise_all(mean)
+
+
   lambda <- df.fit %>%
     select(chain, matches("^lambda\\[")) %>%
     summarise_all(mean)
@@ -96,9 +130,13 @@ for(i in 1:length(res_list)) {
     select(chain, matches("^tau\\[")) %>%
     summarise_all(mean)
 
-  # lambda <- colMeans(df.fit[str_detect(names(df.fit), "lambda\\[")])
-  # tau <- colMeans(df.fit[str_detect(names(df.fit), "tau\\[")])
+  # lambda <- colMeans(df.fit[str_detect(names(df.fit), "alpha\\[")])
+  # tau <- colMeans(df.fit[str_detect(names(df.fit), "beta\\[")])
   # tau <- matrix(tau, nrow = length(lambda))
+
+  if(suppressWarnings(str_detect(evaluate::evaluate("sdat$lv.par[,2]")[[2]], "Error"))) {
+    sdat$lv.par <- cbind(sdat$lv.par[,1], 0)
+  }
 
   comb_lambda <- rbind(c(0, sdat$lv.par[,1]),lambda)
   comb_tau <- rbind(c(0, sdat$lv.par[,2]), tau)
@@ -156,28 +194,174 @@ for(i in 1:length(res_list)) {
   flps_param <- rbind(true = true_param, est = est_param)
 
   # param_list[[i]] <- list(model_type = model_type, lv_param = lv_param, eta_param = eta, mu.eta = mu.eta, flps_param = flps_param)
+
+  eta_bias <-
+    comb_eta %>%
+    gather("etan", "eta_est", -chain) %>%
+    mutate(chain = rep(0:4, 500)) %>%
+    arrange(chain) %>%
+    mutate(
+      true_eta = rep(abs(sdat$true_eta), 5),
+      eta_est = abs(eta_est)
+      ) %>%
+    group_by(chain) %>%
+    summarise(
+      bias = mean(eta_est - true_eta),
+      rmse = sqrt(mean((eta_est - true_eta)^2)),
+    )
+
   param_list[[i]] <-
-    list(model_type = model_type,
+    list(model_type = paste0(model_type, "_", scaley),
          comb_lambda = comb_lambda,
          comb_tau = comb_tau,
          eta_param = comb_eta,
-         flps_param = flps_param)
+         flps_param = flps_param,
+         eta_bias = eta_bias)
 
   print(i)
   gc()
 }
-saveRDS(param_list, "report/rds/irt_1204.rds")
-param_list[[11]]
+saveRDS(param_list, "report/rds/irt_info_1214.rds")
+# param_list[[11]]
+#
+# param_list.2PL <- param_list
+#
+#
+# param_list.sem <- param_list
+# param_list.gpcm <- param_list
+#
+#
+# res_combined_1130 <- list(param_list.sem = param_list.sem,
+#                           param_list.2PL = param_list.2PL,
+#                           param_list.gpcm = param_list.gpcm)
+#
+# saveRDS(res_combined_1130, "report/rds/res_combined_complete_1130.rds")
 
-param_list.2PL <- param_list
+# -------------------------------------------------------------------------
+param_info_list <- readRDS("report/rds/irt_info_1214.rds")
+param_noinfo_list <- readRDS("report/rds/irt_noinfo_1214.rds")
+
+ns <- 13
+ys <- 14
 
 
-param_list.sem <- param_list
-param_list.gpcm <- param_list
+lapply(param_list, "[[", "comb_lambda")
+lapply(param_list, "[[", "eta_param")
+lapply(param_list, "[[", "flps_param") %>%
+  bind_rows(.id = "rep") %>%
+  filter(chain != 0) %>%
+  ggplot() +
+  geom_point(aes(x = as.factor(chain), y = b1, colour = rep),
+             size = 1.2) +
+  geom_hline(yintercept = -0.2, size = 1.2)
+
+a1 <- param_list[[1]]
+
+plotlist <- mk_plot(a1)
+
+plotlist$p_lambda
+plotlist$p_tau
+plotlist$p_eta
+plotlist$p_eta_group
+plotlist$t_param
 
 
-res_combined_1130 <- list(param_list.sem = param_list.sem,
-                          param_list.2PL = param_list.2PL,
-                          param_list.gpcm = param_list.gpcm)
+mk_plot <- function(picked) {
 
-saveRDS(res_combined_1130, "report/rds/res_combined_complete_1130.rds")
+  model_type <- picked$model_type
+
+  p_lambda <- picked$comb_lambda %>%
+    set_names(c("chain", paste0("fs",1:20))) %>%
+    gather("name", "value", -chain) %>%
+    ggplot() +
+    geom_point(aes(name, value,
+                   color = as.factor(chain))) +
+    geom_line(
+      aes(name, value,
+          color = as.factor(chain),
+          linetype = as.factor(chain),
+          group = as.factor(chain)),
+      size = 1.2) +
+    labs(color = "Chain",
+         x = "factor loading",
+         y = "est",
+         title = "Factor loading", subtitle = picked) +
+    theme_bw(base_size = 16)
+
+  p_tau <- picked$comb_tau %>%
+    set_names(c("chain", paste0("int",1:20))) %>%
+    gather("name", "value", -chain) %>%
+    ggplot() +
+    geom_point(aes(name, value,
+                   color = as.factor(chain))) +
+    geom_line(
+      aes(name, value,
+          color = as.factor(chain),
+          linetype = as.factor(chain),
+          group = as.factor(chain)),
+      size = 1.2) +
+    labs(color = "Chain",
+         x = "intercept",
+         y = "est",
+         title = "Intercept (or difficulty)", subtitle = picked) +
+    theme_bw(base_size = 16)
+
+  eta_param <- picked$eta_param %>%
+    gather("name", "value", -chain)
+
+  pop_eta <- eta_param %>% filter(chain == 0)
+  est_eta <- eta_param %>% filter(chain != 0) %>% arrange(chain)
+
+
+
+  est_eta <- est_eta %>%
+    mutate(
+      group = rep(rep(c("trt","ctl"), each = 250), 4),
+      pop_eta = rep(pop_eta$value,4))
+
+
+  p_eta_group <- est_eta %>%
+    ggplot() +
+    geom_point(aes(pop_eta, value,
+                   color = group),
+               alpha = 0.5) +
+    geom_smooth(
+      aes(pop_eta, value,
+          color = group),
+      method='lm', formula= y~x,
+      se = F
+    ) +
+    facet_wrap(chain ~ .) +
+    labs(color = "Chain",
+         y = "Est eta",
+         title = "Correlation between true eta and est eta by group",
+         subtitle = picked) +
+    theme_bw(base_size = 16)
+
+  p_eta <-est_eta %>%
+    ggplot() +
+    geom_point(aes(pop_eta, value,
+                   color = as.factor(chain)),
+               alpha = 0.5) +
+    geom_smooth(
+      aes(pop_eta, value,
+          color = as.factor(chain)),
+      method='lm', formula= y~x,
+      se = F
+    ) +
+    facet_wrap(chain ~ .) +
+    labs(color = "Chain",
+         y = "Est eta",
+         title = "Correlation between true eta and est eta",
+         subtitle = picked) +
+    theme_bw(base_size = 16)
+
+  t_param <- picked$flps_param
+
+  list(p_lambda = p_lambda,
+       p_eta = p_eta,
+       t_param = t_param,
+       p_tau = p_tau,
+       p_eta_group = p_eta_group)
+}
+
