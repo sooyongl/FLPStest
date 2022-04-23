d8986b86c04b99508e14f48cfda866227ed30e2b
Sooyong Lee <sooyongl09@utexas.edu>
2022-04-23 14:52:49 -0500

    Update test_multiple.r
    For running final simulation

--- test/test_multiple.r
+++ test/test_multiple.r
@@ -11,18 +11,18 @@ expand.grid.df <- function(...) Reduce(function(...) merge(..., by=NULL), list(.
 
 # condition ---------------------------------------------------------------
 lvmodel <- c("rasch","2pl","gpcm","grm")
-nsample <- c(100, 2000)
-nitem   <- c(30)
+nsample <- c(500, 1000, 2000)
+nitem   <- c(100)
 fnitem  <- expand.grid(nsample=nsample, nitem=nitem, lvmodel=lvmodel)
 
 nsample <- c(1000)
-nitem   <- c(50, 200)
+nitem   <- c(50, 100, 200)
 fnsize  <- expand.grid(nsample=nsample, nitem=nitem, lvmodel=lvmodel)
 cond_table <- rbind(fnitem,fnsize)
 
 cond_table <- expand.grid.df(cond_table,data.frame(linearity = c(T,F)))
 cond_table <- expand.grid.df(cond_table,data.frame(ydist = c("n","t","t3")))
-cond_table <- expand.grid.df(cond_table,data.frame(rep = 1:10))
+cond_table <- expand.grid.df(cond_table,data.frame(rep = 1:1))
 
 # core setting ------------------------------------------------------------
 cl <- getMPIcluster()
@@ -35,10 +35,11 @@ doParallel::registerDoParallel(cl)
 # condition filter --------------------------------------------------------
 cond_table <-
   cond_table[
-    cond_table$lvmodel == "2pl" & 
-      cond_table$linearity == T & 
-      cond_table$ydist == "n" & 
-      cond_table$nitem %in% c(30),  ]
+    cond_table$linearity == T &
+    cond_table$ydist == "n"
+# cond_table$lvmodel == "2pl" &
+# cond_table$nitem %in% c(30)
+,  ]
 # nrow(cond_table)
 # loop --------------------------------------------------------------------
 oo <- foreach(
@@ -48,7 +49,7 @@ oo <- foreach(
   .export = source_funs
 ) %dopar% {
 
-  # set.seed(i)
+  set.seed(i)
   nsample     <- cond_table$nsample[i]
   nitem       <- cond_table$nitem[i]
   linearity   <- cond_table$linearity[i]
@@ -56,14 +57,17 @@ oo <- foreach(
   lvmodel     <- as.character(cond_table$lvmodel[i])
   rep         <- cond_table$rep[i]
 
+  r2y = 0.2
+  r2eta = 0.5
+
   # generate data ------------------------------------------
   sim_condition <- list(
     N       = nsample, # sample size
-    R2Y     = 0.3, # 0.1 0.2 0.5
+    R2Y     = 0.2, # 0.1 0.2 0.5
     R2eta   = 0.5, # 0.2 0.5 0.75
-    omega   = 0.2, # round(runif(1, 0.1, 0.3),3),
-    tau0    = 0.3, # round(runif(1, 0.2, 0.4),3),
-    tau1    = -0.15, # round(runif(1, -0.2, -0.1),3),
+    omega   = round(runif(1, 0.1, 0.3),3),
+    tau0    = round(runif(1, 0.5, 0.7),3),
+    tau1    = round(runif(1, -0.2, -0.1),3),
     linear  = linearity,
     ydist   = ydist,
     lambda  = 0.6,
@@ -81,7 +85,7 @@ oo <- foreach(
   fit <- rstan::stan(
     file   = stanfilename,
     data   = sdat$stan_dt,
-    chain  = 2,
+    chain  = 3,
     cores  = 1,
     iter   = 6000,
     warmup = 2000
@@ -91,8 +95,8 @@ oo <- foreach(
 
   filename <- paste0(paste(
     "matched",
-    nsample, nitem, lvmodel, linearity, ydist, rep,
-    
+    nsample, nitem, lvmodel, r2y, r2eta, linearity, ydist, rep,
+
     sep = "_"),".rds")
 
   saveRDS(o, file.path("results",filename))
